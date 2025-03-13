255ce95e21659f839ccd037932db3e725fd35bdf
Alex Xu <Da.Alexxu@gmail.com>
2025-03-13 15:08:14 +1300

    Add: File main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# /setup.py
+
+
+# Define Main
+def main() -> None:
+    import Git as git
+    import Terminal as terminal
+
+    git.setup()
+    terminal.setup()
+
+
+if __name__ == "__main__":
+    main()
