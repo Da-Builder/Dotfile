1f4c712f75a54e2021bfc76efad22d982cd41286
Alex Xu <Da.Alexxu@gmail.com>
2025-08-09 17:25:18 +1200

    Update: Git

--- Git/__init__.py
+++ Git/__init__.py
@@ -1,14 +1,21 @@
 # /Git/__init__.py
 
 
-# Define Setup
 def setup() -> None:
-    from pathlib import Path
+	from pathlib import Path
+	from shutil import rmtree
 
-    PATH = "~/.config/git/"
-    if not (path := Path(PATH).expanduser()).exists():
-        path.symlink_to(Path(__file__).parent)
+	# Define Path
+	BASE: Path = Path(__file__).parent / "Configuration"
+	PATH: Path = Path("~/.gitconfig").expanduser()
+
+	# Clean Destination
+	if PATH.exists():
+		PATH.unlink() if not PATH.is_dir() else rmtree(PATH)
+
+	# Create Symbolic Link
+	PATH.symlink_to(BASE)
 
 
 if __name__ == "__main__":
-    setup()
+	setup()
